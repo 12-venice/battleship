e429a66309cac1fdf1cfa64ca9429bd424f58b9a
Maksim Tkachenko <frontendkk1@gmail.com>
2022-03-31 22:26:41 +0300

    Добавлен eslint

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import {Props} from './types';
 import './App.css';
 
-const App:Props = () => {
+export const App:Props = () => {
     return (
         <><h1>Морской бой</h1>
         </>
     );
 }
-
-export default App;
