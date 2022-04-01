4898d9347dc9041715931c30ce37a5f5bd88cc7c
Maksim Tkachenko <frontendkk1@gmail.com>
2022-04-01 13:36:19 +0300

    Добавлены eslint правила

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import {Props} from './types';
+import { Props } from './types';
 import './App.css';
 
-export const App:Props = () => {
-    return (
-        <><h1>Морской бой</h1>
-        </>
-    );
-}
+export const App: Props = () => <h1>Морской бой</h1>;
