6b1a8221170045917282ff4c740fa1d387829d98
Maksim Tkachenko <frontendkk1@gmail.com>
2022-04-01 17:51:14 +0300

    Создан компонент ErrorBoundary

---
+++ src/components/utils/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,32 @@
+import React, {Component, ErrorInfo} from 'react';
+import {Props, State} from './types';
+
+export class ErrorBoundary extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = { error: null, errorInfo: null };
+    }
+
+    componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+        this.setState({
+            error: error,
+            errorInfo: errorInfo
+        })
+    }
+
+    render() {
+        if (this.state.errorInfo) {
+            return (
+                <div>
+                    <h2>Something went wrong.</h2>
+                    <details style={{ whiteSpace: 'pre-wrap' }}>
+                        {this.state.error && this.state.error.toString()}
+                        <br />
+                        {this.state.errorInfo.componentStack}
+                    </details>
+                </div>
+            );
+        }
+        return this.props.children;
+    }
+}
