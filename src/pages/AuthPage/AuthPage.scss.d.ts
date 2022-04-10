bdeb649ca711b46039ee5602f5ffa5ebab62e477
Andrey Sharov <drzoiberg2005@yandex.ru>
2022-04-10 23:26:17 +0300

    feat(issue14): Страница пользователя, страница регистрации, валидация форм (#39)
    * fixed form, add validation, add register page
    
    * add profile page
    
    * some fix home page
    
    * corrected remarks
    
    * corrected remarks
    
    * corrected remarks
    
    * corrected remarks

--- src/pages/AuthPage/AuthPage.scss.d.ts
+++ src/pages/AuthPage/AuthPage.scss.d.ts
@@ -1,4 +1,6 @@
 declare const styles: {
     readonly auth__header: string;
+    readonly auth__link: string;
+    readonly auth__main: string;
 };
 export = styles;
--- src/pages/HomePage/HomePage.scss.d.ts
+++ src/pages/HomePage/HomePage.scss.d.ts
@@ -1,13 +1,13 @@
 declare const styles: {
-    readonly block__buttons: string;
-    readonly block__header: string;
-    readonly block__main: string;
-    readonly block__menu: string;
-    readonly block__select: string;
-    readonly 'block__select-toggle-logotype': string;
-    readonly 'block__select-type-logotype': string;
-    readonly image__left: string;
-    readonly image__right: string;
+    readonly home__buttons: string;
+    readonly home__header: string;
+    readonly 'home__image-left': string;
+    readonly 'home__image-right': string;
+    readonly home__main: string;
+    readonly home__menu: string;
+    readonly home__select: string;
+    readonly 'home__select-toggle-logotype': string;
+    readonly 'home__select-type-logotype': string;
     readonly selected: string;
 };
 export = styles;
