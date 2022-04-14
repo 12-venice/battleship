402348597616193343ec02f9911ab469e5c97aa9
RX <fpreox@ya.ru>
2022-04-14 17:21:06 +0300

    removed the test code

--- src/components/utils/Routes/Routes.tsx
+++ src/components/utils/Routes/Routes.tsx
@@ -5,7 +5,6 @@ import { HomePage } from 'src/pages/HomePage';
 import { NotFoundPage } from 'src/pages/NotFoundPage';
 import { ProfilePage } from 'src/pages/ProfilePage';
 import { RegisterPage } from 'src/pages/RegisterPage';
-import { Button } from 'src/components/Button';
 import { PageLinks } from './types';
 
 export const Routes = (): JSX.Element => (
@@ -24,19 +23,6 @@ export const Routes = (): JSX.Element => (
         </Route>
         <Route exact path={PageLinks.forum}>
             <ForumPage />
-        </Route>
-        <Route exact path="/btn">
-            <Button title="SIGN IN!" skin="wide" />
-            <Button title="SEND" skin="short" />
-            <Button title="RESET" skin="short" color="green" />
-            <Button title="LEADERS" skin="regular" />
-            <Button title="FORUM" color="yellow" />
-            <Button title="X" skin="quad" color="red" />
-            <Button title="i" skin="quad" />
-            <Button title="PLAY!" skin="large" color="green" disabled />
-            <Button title="CLOSE" skin="high" color="yellow" />
-            <Button title="LOG OUT" skin="auth" color="blue" />
-            <Button title="LOG IN" skin="auth" noFill />
         </Route>
         <Route>{NotFoundPage}</Route>
     </Switch>
