6f3b0a7641451681cc752f8a71c58f2beeb9802b
Maksim Tkachenko <frontendkk1@gmail.com>
2022-04-15 21:28:18 +0300

    format

--- src/pages/GamePage/components/PlayerName/PlayerName.tsx
+++ src/pages/GamePage/components/PlayerName/PlayerName.tsx
@@ -1,16 +1,28 @@
 import cn from 'classnames';
 import styles from './PlayerName.scss';
-import {Props}from './types'
+import { Props } from './types';
 
-export const PlayerName = ({ avatarSrc, avatarPosition = 'left', name }: Props) => (
-  <div className={styles.container}>
-      <div
-        className={cn(styles.flex, {
-            [styles.avatarLeft]: avatarPosition === 'right',
-        })}
-      >
-          {avatarSrc ? <div className={styles.avatar}><img src={avatarSrc} alt=""/></div> : <div className={styles.avatar}><p className={styles.nameCharacter}>{name.charAt(0)}</p></div>}
-          <p className={styles.name}>{name}</p>
-      </div>
-  </div>
+export const PlayerName = ({
+    avatarSrc,
+    avatarPosition = 'left',
+    name,
+}: Props) => (
+    <div className={styles.container}>
+        <div
+            className={cn(styles.flex, {
+                [styles.avatarLeft]: avatarPosition === 'right',
+            })}
+        >
+            {avatarSrc ? (
+                <div className={styles.avatar}>
+                    <img src={avatarSrc} alt="" />
+                </div>
+            ) : (
+                <div className={styles.avatar}>
+                    <p className={styles.nameCharacter}>{name.charAt(0)}</p>
+                </div>
+            )}
+            <p className={styles.name}>{name}</p>
+        </div>
+    </div>
 );
