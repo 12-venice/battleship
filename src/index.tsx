5e5d292d56eb0d17554533e24b9457a4e45de401
RX <fpreox@ya.ru>
2022-04-19 00:16:29 +0300

    create testing store

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './components/App';
+import { configureStore } from './store/store';
+
+const store = configureStore({});
+
+console.log(store.getState());
+console.log(store.getState().user);
 
 ReactDOM.render(
     <BrowserRouter>
