a4583cd10df8e26392f35c0212ffc9335ba25f28
Andrey Sharov <drzoiberg2005@yandex.ru>
2022-04-20 01:13:02 +0300

    make it better

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,14 @@
 import { AuthProvider } from 'src/context/Authprovider';
+import { ErrorBoundary } from '../utils/ErrorBoundary';
 import { useRoutes } from '../utils/Routes';
 import './App.scss';
 
 export const App = (): JSX.Element => {
     const routes = useRoutes();
 
-    return <AuthProvider>{routes}</AuthProvider>;
+    return (
+        <AuthProvider>
+            <ErrorBoundary>{routes}</ErrorBoundary>
+        </AuthProvider>
+    );
 };
