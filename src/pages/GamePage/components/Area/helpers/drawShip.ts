51f64781c67470f35e77965702492eb38e2b0c47
Maksim Tkachenko <frontendkk1@gmail.com>
2022-04-21 17:20:27 +0300

    Area

---
+++ src/pages/GamePage/components/Area/helpers/drawShip.ts
@@ -0,0 +1,24 @@
+import { shipsSrcMap } from 'src/pages/GamePage/data';
+
+export const drawShip = ({
+    ctx,
+    x,
+    y,
+    type,
+    cellSize,
+    isHorizontal = false,
+}) => {
+    const image = new Image(cellSize, cellSize * type);
+    image.src = shipsSrcMap.get(type);
+    image.onload = () => {
+        if (isHorizontal) {
+            ctx.save();
+            ctx.translate(y, x);
+            ctx.rotate((Math.PI / 180) * 90);
+            ctx.drawImage(image, -5, -(cellSize * type - 3));
+            ctx.restore();
+        } else {
+            ctx.drawImage(image, x, y);
+        }
+    };
+};
--- src/pages/GamePage/components/Area/types.ts
+++ src/pages/GamePage/components/Area/types.ts
@@ -1,6 +1,30 @@
+import { RefObject } from 'react';
+import type { Matrix, DeckCount } from 'src/gameCore/Field/types';
+
+export type CanvasCtx = CanvasRenderingContext2D;
+
+export type Ship = {
+    /** Уникальный ключ */
+    id: string;
+    /** Количество палуб */
+    deckCount: DeckCount;
+    /** Координата по горизонтали */
+    x: number;
+    /** Координата по вертикали */
+    y: number;
+    /** Направление корабля */
+    isHorizontal?: boolean;
+};
+
 export type Props = {
     /** Ширина игрового поля. От него зависит размер ячеек */
     areaWidth: number;
+    /** Ссылка на ref с полем в canvas */
+    canvasRef: RefObject<HTMLCanvasElement>;
+    /** Матрица с состояниями ячеек */
+    matrix?: Matrix;
+    /** Расположение кораблей */
+    ships?: Ship[];
     /** Цвет заливки ячеек. По умолчанию белый */
     fillColor?: string;
 };
