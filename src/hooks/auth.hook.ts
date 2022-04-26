a8dc5fbb925d55a5869885b3cbb17da80349eb00
Andrey Sharov <drzoiberg2005@yandex.ru>
2022-04-27 00:54:15 +0300

    fixed redirect after restart game page

--- src/hooks/auth.hook.ts
+++ src/hooks/auth.hook.ts
@@ -1,5 +1,4 @@
 import { useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { userService } from 'src/store/services/userService';
 import { useHttp } from './http.hook';
 
