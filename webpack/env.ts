368cecaf72f7c2da3538b2c6e8d215a8ec708f90
Andrei Sharov <asharov@sitronics.com>
2022-06-07 11:15:30 +0300

    refactor commands

--- webpack/env.ts
+++ webpack/env.ts
@@ -6,8 +6,8 @@ dotenv.config();
 const IS_DEV = process.env.NODE_ENV !== 'production';
 const SRC_DIR = path.join(__dirname, '../src');
 const DIST_DIR = path.join(__dirname, '../dist');
-const DB = process.env.DB_CONN;
+const DB = process.env.DB_CONN || '';
 const { PORT } = process.env;
-const IS_DEV_SERVER = process.env.DEV === 'true';
+const IS_DEV_SERVER = process.env.SERVER === 'true';
 
 export { IS_DEV, SRC_DIR, DIST_DIR, DB, PORT, IS_DEV_SERVER };
