93a771c81b5e4da7489910473fa1054a4f6cc801
Andrey Sharov <drzoiberg2005@yandex.ru>
2022-06-09 06:55:30 +0300

    commit

--- webpack/server.config.ts
+++ webpack/server.config.ts
@@ -44,6 +44,11 @@ const config: Configuration = {
         new MiniCssExtractPlugin({
             filename: '[name].css',
         }),
+        new NodemonPlugin({
+            script: './dist/server.js',
+            ignore: ['main.js', '*.js.map'],
+            delay: 1,
+        }),
     ],
 
     performance: {
