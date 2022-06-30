e5dc2161d42cd034be30d4ec30683e9fc694bd19
RX <fpreox@ya.ru>
2022-06-30 15:20:13 +0300

    add information

--- src/components/Information/information.tsx
+++ src/components/Information/information.tsx
@@ -3,6 +3,11 @@ import { useSelector } from 'react-redux';
 import { Button } from 'src/components/Button';
 import { ModalWindow } from 'src/components/ModalWindow';
 import { AllStateTypes } from 'src/store/reducers';
+import infoAuth from '../../../images/infoAuth.png';
+import infoFind from '../../../images/infoFind.png';
+import infoStart from '../../../images/infoStart.png';
+import infoSwitch from '../../../images/infoSwitch.png';
+
 import styles from './information.scss';
 import { Props } from './types';
 
@@ -12,18 +17,111 @@ export const Information: Props = ({ close }): JSX.Element => {
     );
     return (
         <ModalWindow>
-            <h2 className={styles.information__label}>
-                {data.labels.information}
-            </h2>
-            <p className={styles.information__description}>
-                {data.text.information}
-            </p>
-            <Button
-                skin="high"
-                color="green"
-                title={data.buttons.back}
-                onClick={close}
-            />
+            <div className={styles.information__block}>
+                <h2 className={styles.information__label}>
+                    {data.labels.information}
+                </h2>
+                <p className={styles.information__description}>
+                    {data.text.information1}
+                </p>
+                <h2 className={styles.information__label}>
+                    {data.text.information2}
+                </h2>
+                <p className={styles.information__description}>
+                    {data.text.information3}
+                </p>
+                <p className={styles.information__description}>
+                    {data.text.information4}
+                </p>
+                <h2 className={styles.information__label}>
+                    {data.text.information5}
+                </h2>
+                <p className={styles.information__description}>
+                    {data.text.information6}
+                </p>
+                <img
+                    className={styles.information__img}
+                    src={infoAuth}
+                    alt="Как авторизироваться"
+                />
+                <p className={styles.information__description}>
+                    {data.text.information7}
+                </p>
+                <p className={styles.information__description}>
+                    {data.text.information8}
+                </p>
+                <img
+                    className={styles.information__img}
+                    src={infoFind}
+                    alt="Как найти соперника"
+                />
+                <p className={styles.information__description}>
+                    {data.text.information9}
+                </p>
+                <p className={styles.information__description}>
+                    {data.text.information10}
+                </p>
+                <h2 className={styles.information__label}>
+                    {data.text.information11}
+                </h2>
+                <p className={styles.information__description}>
+                    {data.text.information12}
+                </p>
+                <img
+                    className={styles.information__img}
+                    src={infoStart}
+                    alt="Как расставить корабли"
+                />
+                <p className={styles.information__description}>
+                    {data.text.information13}
+                </p>
+                <p className={styles.information__description}>
+                    {data.text.information14}
+                </p>
+                <p className={styles.information__description}>
+                    {data.text.information15}
+                </p>
+                <img
+                    className={styles.information__img}
+                    src={infoSwitch}
+                    alt="Как расставить корабли"
+                />
+                <p className={styles.information__description}>
+                    {data.text.information16}
+                </p>
+                <p className={styles.information__description}>
+                    {data.text.information17}
+                </p>
+                <h2 className={styles.information__label}>
+                    {data.text.information18}
+                </h2>
+                <p className={styles.information__description}>
+                    {data.text.information19}
+                </p>
+                <p className={styles.information__description}>
+                    {data.text.information20}
+                </p>
+                <p className={styles.information__description}>
+                    {data.text.information21}
+                </p>
+                <p className={styles.information__description}>
+                    {data.text.information22}
+                </p>
+                <p className={styles.information__description}>
+                    {data.text.information23}
+                </p>
+                <p className={styles.information__description}>
+                    {data.text.information24}
+                </p>
+                <div className={styles.information__footer}>
+                    <Button
+                        skin="high"
+                        color="green"
+                        title={data.buttons.back}
+                        onClick={close}
+                    />
+                </div>
+            </div>
         </ModalWindow>
     );
 };
--- src/store/reducers/EN.ts
+++ src/store/reducers/EN.ts
@@ -60,8 +60,55 @@ export const ENGLISH = {
         editComment: 'Describe your comment',
         add: 'Describe your topic',
         avatar: 'Добавь аватар со своего компьютера',
-        information:
-            '"BATTLESHIP" is a game for two participants in which players take turns calling coordinates on an unknown opponent\'s map. If the opponent has a ship at these coordinates (the coordinates are occupied), then the ship or part of it is "drowned", and the hit gets the right to make another move. The player\'s goal is to sink all enemy ships first.',
+        information1: `We invite you to have a great time in our application!
+        Here you can try your hand against one of our
+        mighty pirate robots or play against a real
+        human:)`,
+        information2: 'Battleship is...',
+        information3: `is a two-person game in which players take turns
+        call the coordinates on the opponent's map, which they do not know. If
+        the opponent has a ship at these coordinates (coordinates
+        occupied), the ship or its part is "sunk", and the hitter
+        gets the right to make one more move. The player's goal is to be the first
+        to sink all of the opponent's ships.`,
+        information4: `Each player's field consists of 100 cells (10 by 10). В
+        At the beginning of the game it is necessary to place 10 ships: 4
+        single-deck, 3 double-deck, 2 triple-deck, 1
+        4 decks. Note that there must always be one square between the ships.
+        there must be one cell between the ships.`,
+        information5: `We know the rules, let's play!`,
+        information6: 'You need to log in to get started.',
+        information7: `Are you logged in? Great! You can choose a game mode on the
+        main page and start playing!`,
+        information8:
+            'If you choose the online mode, you need to find an opponent.',
+        information9: 'Choose an opponent yourself or press the RANDOM button.',
+        information10: 'The player will receive an invitation from you, wait :)',
+        information11: 'Start of the game',
+        information12: `It's time to deploy the ships! You can do this automatically
+        or manually by dragging each ship. To
+        Drag and drop a ship onto the field and right-click on it.`,
+        information13: 'Game on!',
+        information14: `Fifteen seconds are given for each move. If you don't make it in time, your turn
+        goes to your opponent.`,
+        information15: `The fields change automatically, but you can switch them
+        yourself with the button:`,
+        information16: `To make online nerfing even more interesting, this
+        chat mode is available!`,
+        information17: `Don't get carried away, time is ticking! :)`,
+        information18: 'Other',
+        information19: `Points earned online are saved. You can compare your
+        score against other players on the
+        leaderboard!`,
+        information20: `Discuss the game, thank the developers, point out
+        find a bug, leave a review or share an interesting
+        topic you can on our forum!`,
+        information21: `If you start a fascinating conversation during the game, you can
+        continue on the built-in messenger page!`,
+        information22: `Each player can add and edit his data.
+        To do this, go to your profile.`,
+        information23: 'The app is available in two languages.',
+        information24: 'We wish you a pleasant game!',
         togglemode: 'Toggle play mode!',
         forum: 'Send comment...',
         deleteTopic: 'Do you want to delete the topic?',
